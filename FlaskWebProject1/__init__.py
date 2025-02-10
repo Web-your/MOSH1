ac0c7f47074bcdca59fe13fe5181026aa0cc7dd6
Web-your <ermakyur2008@gmail.com>
2025-02-10 18:16:27 +0300

    Update __init__.py

--- FlaskWebProject1/__init__.py
+++ FlaskWebProject1/__init__.py
@@ -1,8 +1,14 @@
 import cv2
+import os
 import numpy as np
+from midiutil import MIDIFile
 from flask import Flask, render_template, request, send_file, redirect
+from werkzeug.utils import secure_filename
 import sqlite3
 app = Flask(__name__)
+
+UPLOAD_FOLDER = r'C:\Users\Mi\PycharmProjects\MOSH\FlaskWebProject1\static\pic'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def cuncon():
     conn = sqlite3.connect(r'C:\Users\Mi\PycharmProjects\MOSH\FlaskWebProject1\static\Data_Baze\NotesInf.db')
@@ -13,6 +19,8 @@ def cuncon():
 def NoteTrans(file_path):
 
     img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    print(img)
+    print(file_path)
 
     _, img_thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
 
@@ -103,14 +111,22 @@ def NoteTrans(file_path):
             note_type = 'e'
         elif (dY > k * 25 and dY < k * 27):
             note_type = 'e1'
-        elif ((dY > k * -13 and dY < k * -11) or (dY > k * 31 and dY < k * 33)):
+        elif (dY > k * -13 and dY < k * -11):
             note_type = 'f'
-        elif ((dY > k * -6 and dY < k * -4) or (dY > k * 37 and dY < k * 39)):
+        elif (dY > k * 31 and dY < k * 33):
+            note_type = 'f1'
+        elif (dY > k * -6 and dY < k * -4):
             note_type = 'g'
-        elif ((dY > k * 0 and dY < k * 2) or (dY > k * 43 and dY < k * 45)):
+        elif (dY > k * 37 and dY < k * 39):
+            note_type = 'g1'
+        elif (dY > k * 0 and dY < k * 2):
             note_type = 'a'
-        elif ((dY > k * 6 and dY < k * 8) or (dY > k * 49 and dY < k * 51)):
+        elif (dY > k * 43 and dY < k * 45):
+            note_type = 'a1'
+        elif (dY > k * 6 and dY < k * 8):
             note_type = 'h'
+        elif (dY > k * 49 and dY < k * 51):
+            note_type = 'h1'
         cont[1] = note_type
 
     # add note type to output
@@ -127,20 +143,64 @@ def NoteTrans(file_path):
     print(notes)
     return notes
 
+def MidiNote(notes):
+    SlovNote = {'c': 60, 'd': 62, 'e': 64, 'f': 65, 'g': 67, 'a': 69, 'h': 71, 'c1': 72, 'd1': 74, 'e1': 76, 'f1': 77,
+                'g1': 79, 'a1': 81, 'h1': 83}
+    finaly = []
+    for i in notes:
+        finaly.append(SlovNote[i])
+    print(finaly)
+    degrees = finaly  # MIDI note number
+    track = 0
+    channel = 0
+    time = 0  # In beats
+    duration = 1  # In beats
+    tempo = 60  # In BPM
+    volume = 100  # 0-127, as per the MIDI standard
+
+    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track
+    # automatically created)
+    MyMIDI.addTempo(track, time, tempo)
+
+    for pitch in degrees:
+        MyMIDI.addNote(track, channel, pitch, time, duration, volume)
+        time = time + 1
+
+    with open("C:/Users/Mi/PycharmProjects/MOSH/FlaskWebProject1/static/Midi/CC.mid", "wb") as output_file:
+        MyMIDI.writeFile(output_file)
+
+
 
 
 
 # Create index function for upload and return files
-@app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     conn = cuncon()[1]
-    if request.method == 'POST':
+    if request.method == "POST":
         file = request.files['file']
-        conn.execute(f"INSERT INTO Notes ( Filename, picture) VALUES ( '{file.filename}', '{file.read()}')")
-        conn.commit()
-        int1 = NoteTrans(file.read())
-        print(int1)
+        if file:
+            print("yes")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(filename)
+            int1 = NoteTrans(f'C:/Users/Mi/PycharmProjects/MOSH/FlaskWebProject1/static/pic/{filename}')
+            MidiNote(int1)
+
+
+        return redirect("home", 301)
+    return render_template('layout.html')
+@app.route('/home', methods=['GET', 'POST'])
+def homeindex():
+    conn = cuncon()[1]
+    if request.method == "POST":
+        file = request.files['file']
+        print("yes")
+        if file and file.filename.endswith('.csv'):
+            print("yes")
+            int1 = NoteTrans(file.filename)
+            print(int1)
+
 
         return redirect("home", 301)
     return render_template('layout.html')
